5252edf2c5cfb7003a804d0e74482119dccb7f95
jordanyazzie <jordanyazzie@mail.weber.edu>
2019-04-14 12:31:42 -0600

    Final Project Progress

--- app.py
+++ app.py
@@ -1,62 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 
-@app.route('/mystery')
-def mystery():
-    return render_template('mystery.html')
-
-@app.route('/mysteryBookOne')
-def mysterybookone():
-    return render_template('mysteryBookOne.html')
-
-@app.route('/mysteryBookTwo')
-def mysterybooktwo():
-    return render_template('mysteryBookTwo.html')
-
-@app.route('/mysteryBookThree')
-def mysterybookthree():
-    return render_template('mysteryBookThree.html')
 
 
-@app.route('/nonFiction')
-def nonfiction():
-    return render_template('nonFiction.html')
-
-@app.route('/nonFictionBookOne')
-def nonfictionbookone():
-    return render_template('nonFictionBookOne.html')
-
-@app.route('/nonFictionBookTwo')
-def nonfictionbooktwo():
-    return render_template('nonFictionBookTwo.html')
-
-@app.route('/nonFictionBookThree')
-def nonfictionbookthree():
-    return render_template('nonFictionBookThree.html')
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'YouAre' or request.form['password'] != 'Awesome':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
 
-@app.route('/poetry')
-def poetry():
-    return render_template('poetry.html')
 
-@app.route('/poetryBookOne')
-def poetrybookone():
-    return render_template('poetryBookOne.html')
 
-@app.route('/poetryBookTwo')
-def poetrybooktwo():
-    return render_template('poetryBookTwo.html')
-
-@app.route('/poetryBookThree')
-def poetrybookthree():
-    return render_template('poetryBookThree.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+#
+# @app.route('/mystery')
+# def mystery():
+#     return render_template('mystery.html')
+#
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
